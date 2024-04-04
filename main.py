d0829e92e7bb2ec916c8a0113e887c4e2ee963fa
stivenson <jerxonmjair@gmail.com>
2024-04-04 15:55:28 -0500

    maneras de declarar

---
+++ main.py
@@ -0,0 +1,12 @@
+print ("hola mundo")
+variabl = 15
+
+print (variabl)
+a = 7+7 
+print (a) 
+dinero1 = 100
+dinero2 = 200
+dinero_total = dinero1 + dinero2
+deudas = -200
+d = dinero_total - deudas
+print (d)
